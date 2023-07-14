e61f1c5715be3933c81cc8c8c3348a6d986c768c
Savindu Shehan <savindushehansamaraweera@gmail.com>
2023-07-14 13:29:00 +0530

    create mobile phone

--- Assert/login.php
+++ Assert/login.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>ATOM BANK</title>
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
@@ -13,7 +15,133 @@
                 --div-color4:#C1C8E4;
                 --div-color5:#8860D0;
             }
+            @media only screen and (max-width:1279px){
+                body{
+               display: flex;
+               flex-direction: column;
+               justify-content: center;
+               align-items: center;
+               margin-top: 10vh;
+            }
+            main{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+            .side1{
+                width: 20rem;
+                display: none;
+            }
+            .side2{
+                width: 20rem;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                background-image: linear-gradient(var(--div-color3),var(--div-color2));
+            }
+            .menu{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                text-align: center;
+                width: 100%;
+            }
+             .menu h2{
+                margin-top: 0;
+                display: none;
+                color: var(--div-color4);
+                font-size: 25px;
+                width: 20rem;
+                border-radius: 10px;
+                font-family: 'Roboto', sans-serif;
+                cursor: pointer;
+            }
+              form{
+                width: 60%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            .row1,.row2{
+                display: flex;
+                flex-direction: column;
+            }
+            label{
+                text-align: center;
+                font-family: 'Roboto', sans-serif;
+                font-size: 28px;
+                margin-top: 2.3rem;
+                margin-bottom: 1rem;
+                color: var(--div-color1);
+            }
+            input{
+                margin-top: 1rem;
+                width: 300px;
+                height: 50px;
+                border: none;
+                border-color: var(--div-color4);
+                border-radius: 10px;
+                font-size: 20px;
+                color: var(--div-color1);
+                outline: none;
+            }
+             h1{
+                font-family: 'Roboto', sans-serif;
+                font-size: 40px;
+                color: var(--div-color1);
+            }
+            p{
+                font-family: 'Roboto', sans-serif;
+                text-align: center;
+                line-height: 25px;
+                margin-left: 1rem;
+                margin-right: 1rem;
+                font-size: 20px;
+            }
+            .three{
+                margin-top: 1rem;
+                display: flex;
+                flex-direction: column;
+                margin-left:15rem ;
+                cursor: pointer;
+            }
+            h4{
+                text-align: right;
+                margin-top: 0;
+                margin-bottom: 5px;
+                width: 4rem;
+                height: 0.5rem;
+                background-color:var(--div-color4);
+            }
+             h3{
+                margin-top: 1rem;
+                margin-bottom: 0;
+                font-family: 'Roboto', sans-serif;
+                color: red;
+                font-size: 14px;
+                display: none;
+            }
+            #btn{
+                margin-top: 2.5rem;
+                font-family: 'Roboto', sans-serif;
+                font-size: 45px;
+                color: var(--div-color5);
+                border: none;
+                border-radius: 16px;
+                cursor: pointer;
+                transition: 0.6s;
+                margin-bottom: 3rem;
+            }
 
+            }
+            
+            @media only screen and (min-width:1280px){
             body{
                 margin: 0;
                 padding: 0;
@@ -111,7 +239,10 @@
                 font-size: 14px;
                 display: none;
             }
-            
+            .three{
+                display: none;
+            }
+        }
 
         </style>
     </head>
@@ -122,6 +253,11 @@
                     <img src="../Images in saly/Saly-16.png" alt="image">
                 </div>
                 <div class="side2">
+                <div class="three" id="three">
+                    <h4></h4>
+                    <h4></h4>
+                    <h4></h4>
+                </div>
                     <div class="menu">
                         <h2 id="m1">ATOM</h2>
                         <h2 id="m2">Home</h2>
@@ -155,6 +291,7 @@
             var BTN=document.getElementById("btn");
             var E1=document.getElementById("e1");
             var E2=document.getElementById("e2");
+            var B=document.getElementById("three");
 
             M1.onmousemove=function(){
                 M1.style.backgroundColor="var(--div-color5)";
@@ -197,6 +334,20 @@
                 }
                 else if(U.value==""){
                     E1.style.display="block";
+                }
+            }
+
+            B.onclick = function() {
+                if (M1.style.display === "none") {
+                M1.style.display = "block";
+                M2.style.display = "block";
+                M3.style.display = "block";
+                M4.style.display = "block";
+                } else {
+                M1.style.display = "none";
+                M2.style.display = "none";
+                M3.style.display = "none";
+                M4.style.display = "none";
                 }
             }
                 
